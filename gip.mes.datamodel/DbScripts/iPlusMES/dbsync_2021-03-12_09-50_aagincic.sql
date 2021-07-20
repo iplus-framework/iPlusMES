6bebee9f206d49e1115556d5abda833d8b22f58a
Aleksandar Agincic <aleksandar.aigncic@gmail.com>
2021-07-20 14:26:19 +0200

    PlanningMR add table

--- gip.mes.datamodel/DbScripts/iPlusMES/dbsync_2021-03-12_09-50_aagincic.sql
+++ gip.mes.datamodel/DbScripts/iPlusMES/dbsync_2021-03-12_09-50_aagincic.sql
@@ -15,6 +15,7 @@ CREATE TABLE [dbo].[MDCountrySalesTaxMaterial](
 	[MDCountrySalesTaxMaterialID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
 
 CREATE TABLE [dbo].[MDCountrySalesTaxMDMaterialGroup](
 	[MDCountrySalesTaxMDMaterialGroupID] [uniqueidentifier] NOT NULL,
