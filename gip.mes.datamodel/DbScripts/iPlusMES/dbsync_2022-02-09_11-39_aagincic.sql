d5bcd05d14d862ac6e9b3da3bd6ed1d5a2c722ea
aagincic <aleksandar.agincic@gmail.com>
2022-02-09 11:58:53 +0100

    varchar(20) -> varchar(30) for: DeliveryNoteNo, InOrderNo, OutOrderNo, PickingNo

---
+++ gip.mes.datamodel/DbScripts/iPlusMES/dbsync_2022-02-09_11-39_aagincic.sql
@@ -0,0 +1,4 @@
+alter table DeliveryNote alter column DeliveryNoteNo varchar(30) not null
+alter table InOrder alter column InOrderNo varchar(30) not null
+alter table OutOrder alter column OutOrderNo varchar(30) not null
+alter table Picking alter column PickingNo varchar(30) not null
