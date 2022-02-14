15dfa0e52dfb8b026176fdd6533d2e15fb582b57
Ivan Hrastinski <ihrastinski@kajbum.com>
2022-02-14 14:58:43 +0100

    Work tasks on hold

---
+++ gip.bso.manufacturing/Manufacturing/WorkCenterSelector/BSOWorkTaskOnHold.cs
@@ -0,0 +1,179 @@
+﻿using gip.core.autocomponent;
+using gip.core.datamodel;
+using gip.mes.datamodel;
+using gip.mes.processapplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gip.bso.manufacturing
+{
+    [ACClassInfo(Const.PackName_VarioManufacturing, "en{'Tasks on hold'}de{'Aufgaben in der Warteschleife'}", Global.ACKinds.TACBSO, Global.ACStorableTypes.NotStorable, true, true, SortIndex = 500)]
+    public class BSOWorkTaskOnHold : BSOWorkCenterChild
+    {
+        #region c'tors
+
+        public BSOWorkTaskOnHold(core.datamodel.ACClass acType, IACObject content, IACObject parentACObject, ACValueList parameter, string acIdentifier = "") : 
+            base(acType, content, parentACObject, parameter, acIdentifier)
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        private ACRef<IACComponent> _PAFWorkTaskScan;
+
+        private WorkTaskOnHoldItem _SelectedWorkTaskOnHold;
+        [ACPropertySelected(700, "WorkTask")]
+        public WorkTaskOnHoldItem SelectedWorkTaskOnHold
+        {
+            get => _SelectedWorkTaskOnHold;
+            set
+            {
+                _SelectedWorkTaskOnHold = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<WorkTaskOnHoldItem> _WorkTaskHoldList;
+        [ACPropertyList(700, "WorkTask")]
+        public List<WorkTaskOnHoldItem> WorkTaskOnHoldList
+        {
+            get => _WorkTaskHoldList;
+            set
+            {
+                _WorkTaskHoldList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void Activate(ACComponent selectedProcessModule)
+        {
+            base.Activate(selectedProcessModule);
+
+            if (selectedProcessModule == null)
+                return;
+
+            var childComponents = selectedProcessModule.ACComponentChildsOnServer;
+            var pafWorkTask = childComponents.FirstOrDefault(c => typeof(PAFWorkTaskScanBase).IsAssignableFrom(c.ComponentClass.ObjectType));
+            if (pafWorkTask == null)
+            {
+                Messages.Error(this, "The PAFWorkTaskScan can not be found!", true, null);
+            }
+
+            _PAFWorkTaskScan = new ACRef<IACComponent>(pafWorkTask, this);
+
+            RefreshWorkTasks();
+        }
+
+        public override void DeActivate()
+        {
+            base.DeActivate();
+
+            if (_PAFWorkTaskScan != null)
+            {
+                _PAFWorkTaskScan.Detach();
+                _PAFWorkTaskScan = null;
+            }
+        }
+
+        [ACMethodInfo("", "en{'Start work task'}de{'Arbeitsaufgabe starten'}", 710, true)]
+        public void StartWorkTask()
+        {
+            if (_PAFWorkTaskScan == null)
+                return;
+
+            IACComponent paf = _PAFWorkTaskScan.ValueT;
+            Msg result = paf.ExecuteMethod(nameof(PAFWorkTaskScanBase.OccupyReleaseProcessModule), SelectedWorkTaskOnHold.WFACUrl, SelectedWorkTaskOnHold.ForRelease) as Msg;
+            if (result != null && result.MessageLevel > eMsgLevel.Info)
+            {
+                Messages.Msg(result);
+            }
+
+            RefreshWorkTasks();
+        }
+
+        public bool IsEnabledStartWorkTask()
+        {
+            return SelectedWorkTaskOnHold != null && !SelectedWorkTaskOnHold.ForRelease;
+        }
+
+        [ACMethodInfo("", "en{'Complete work task'}de{'Arbeitsaufgabe abschließen'}", 711, true)]
+        public void CompleteWorkTask()
+        {
+            if (_PAFWorkTaskScan == null)
+                return;
+
+            IACComponent paf = _PAFWorkTaskScan.ValueT;
+            Msg result = paf.ExecuteMethod(nameof(PAFWorkTaskScanBase.OccupyReleaseProcessModule), SelectedWorkTaskOnHold.WFACUrl, SelectedWorkTaskOnHold.ForRelease) as Msg;
+            if (result != null && result.MessageLevel > eMsgLevel.Info)
+            {
+                Messages.Msg(result);
+            }
+
+            RefreshWorkTasks();
+        }
+
+        public bool IsEnabledCompleteWorkTask()
+        {
+            return SelectedWorkTaskOnHold != null && SelectedWorkTaskOnHold.ForRelease;
+        }
+
+        [ACMethodInfo("", "en{'Refresh work tasks'}de{'Arbeitsaufgaben auffrischen'}", 711, true)]
+        public void RefreshWorkTasks()
+        {
+            if (_PAFWorkTaskScan == null)
+                return;
+
+            IACComponent pafWorkTask = _PAFWorkTaskScan.ValueT;
+
+            WorkTaskScanResult prodOrderWFInfoList = pafWorkTask.ExecuteMethod(nameof(PAFWorkTaskScanBase.GetOrderInfos)) as WorkTaskScanResult;
+
+            List<WorkTaskOnHoldItem> result = new List<WorkTaskOnHoldItem>();
+
+            if (prodOrderWFInfoList != null && prodOrderWFInfoList.OrderInfos != null && prodOrderWFInfoList.OrderInfos.Any())
+            {
+                Guid[] temp = prodOrderWFInfoList.OrderInfos.Select(t => t.POPPosId).ToArray();
+
+                using (DatabaseApp dbApp = new DatabaseApp())
+                {
+                    result = dbApp.ProdOrderPartslistPos
+                                  .Include(i => i.ProdOrderPartslist)
+                                  .Include(i => i.ProdOrderPartslist.ProdOrder)
+                                  .Include(i => i.ProdOrderPartslist.Partslist)
+                                  .Where(c => temp.Any(x => x == c.ProdOrderPartslistPosID))
+                                  .OrderBy(c => c.ProdOrderPartslist.ProdOrder.ProgramNo)
+                                  .ThenBy(c => c.ProdOrderPartslist.Partslist.PartslistNo)
+                                  .ThenBy(c => c.Sequence)
+                                  .ToArray()
+                                  .Select(x => new Tuple<ProdOrderPartslistPos, PAProdOrderPartslistWFInfo>(x, prodOrderWFInfoList
+                                                                                                               .OrderInfos.FirstOrDefault(c => x.ProdOrderPartslistPosID == c.POPPosId)))
+                                  .Select(x => new WorkTaskOnHoldItem()
+                                  {
+                                      POProgramNo = x.Item1.ProdOrderPartslist.ProdOrder.ProgramNo,
+                                      StartDate = x.Item2.WFMethodStartDate,
+                                      Sequence = x.Item1.Sequence.ToString(),
+                                      PartslistNo = x.Item1.ProdOrderPartslist.Partslist.PartslistNo,
+                                      PartslistName = x.Item1.ProdOrderPartslist.Partslist.PartslistName,
+                                      IntermediateMaterial = x.Item1.MaterialName,
+                                      WFACUrl = x.Item2.ACUrlWF,
+                                      ForRelease = x.Item2.ForRelease                                 
+                                  })
+                                  .ToList();
+                }
+            }
+
+            WorkTaskOnHoldList = result;
+        }
+
+
+        #endregion
+    }
+}
---
+++ gip.bso.manufacturing/Model/WorkCenter/WorkTaskOnHoldItem.cs
@@ -0,0 +1,114 @@
+﻿using gip.core.datamodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gip.bso.manufacturing
+{
+    [ACClassInfo(Const.PackName_VarioManufacturing, "en{'WorkTaskOnHoldItem'}de{'WorkTaskOnHoldItem'}", Global.ACKinds.TACSimpleClass)]
+    public class WorkTaskOnHoldItem : EntityBase, IACObject
+    {
+        public WorkTaskOnHoldItem()
+        {
+
+        }
+
+        private string _POProgramNo;
+        [ACPropertyInfo(9999)]
+        public string POProgramNo
+        {
+            get => _POProgramNo;
+            set => SetProperty(ref _POProgramNo, value);
+        }
+
+        private DateTime? _StartDate;
+        [ACPropertyInfo(9999)]
+        public DateTime? StartDate
+        {
+            get => _StartDate;
+            set => SetProperty(ref _StartDate, value);
+        }
+
+        private string _PartslistNo;
+        [ACPropertyInfo(9999)]
+        public string PartslistNo
+        {
+            get => _PartslistNo;
+            set => SetProperty(ref _PartslistNo, value);
+        }
+
+        private string _PartslistName;
+        [ACPropertyInfo(9999)]
+        public string PartslistName
+        {
+            get => _PartslistName;
+            set => SetProperty(ref _PartslistName, value);
+        }
+
+        private string _IntermediateMaterial;
+        [ACPropertyInfo(9999)]
+        public string IntermediateMaterial
+        {
+            get => _IntermediateMaterial;
+            set => SetProperty(ref _IntermediateMaterial, value);
+        }
+
+        private string _Sequence;
+        [ACPropertyInfo(9999)]
+        public string Sequence
+        {
+            get => _Sequence;
+            set => SetProperty(ref _Sequence, value);
+        }
+
+        private bool _ForRelease;
+        [ACPropertyInfo(9999)]
+        public bool ForRelease
+        {
+            get => _ForRelease;
+            set => SetProperty(ref _ForRelease, value);
+        }
+
+        public string WFACUrl
+        {
+            get;
+            set;
+        }
+
+        #region IACObject
+
+        public IACObject ParentACObject => null;
+
+        public IACType ACType => this.ReflectACType();
+
+        public IEnumerable<IACObject> ACContentList => this.ReflectGetACContentList();
+
+        public string ACIdentifier => this.ReflectGetACIdentifier();
+
+        public string ACCaption => POProgramNo;
+
+        public object ACUrlCommand(string acUrl, params object[] acParameter)
+        {
+            return this.ReflectACUrlCommand(acUrl, acParameter);
+        }
+
+        public bool IsEnabledACUrlCommand(string acUrl, params object[] acParameter)
+        {
+            return this.ReflectIsEnabledACUrlCommand(acUrl, acParameter);
+        }
+
+        public string GetACUrl(IACObject rootACObject = null)
+        {
+            return this.ReflectGetACUrl(rootACObject);
+        }
+
+        public bool ACUrlBinding(string acUrl, ref IACType acTypeInfo, ref object source, ref string path, ref Global.ControlModes rightControlMode)
+        {
+            return this.ReflectACUrlBinding(acUrl, ref acTypeInfo, ref source, ref path, ref rightControlMode);
+        }
+
+        #endregion
+    }
+}
