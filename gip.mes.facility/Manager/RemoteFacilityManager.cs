9944c715e90f9ca5b75a50120f05f619f0355955
aagincic <aleksandar.agincic@gmail.com>
2022-02-27 09:49:52 +0100

    update RemoteFacilityManager

--- gip.mes.facility/Manager/RemoteFacilityManager.cs
+++ gip.mes.facility/Manager/RemoteFacilityManager.cs
@@ -496,13 +496,14 @@ namespace gip.mes.facility
             }
             else
             {
+                localFC.NotAvailable = changedRemoteFC.NotAvailable;
                 //localFC.CopyFrom(changedRemoteFC, true, false);
-                //MsgWithDetails msgSaveCharge = dbLocal.ACSaveChanges();
-                //if (msgSaveCharge != null)
-                //{
-                //    Messages.Msg(msgSaveCharge);
-                //    successSaveCharge = false;
-                //}
+                MsgWithDetails msgSaveCharge = dbLocal.ACSaveChanges();
+                if (msgSaveCharge != null)
+                {
+                    Messages.Msg(msgSaveCharge);
+                    successSaveCharge = false;
+                }
             }
 
             if (localFC.NotAvailable)
