5a58e2d3c1ec215c446dfe4a3bfa7e38ccc5bac8
aagincic <aleksandar.agincic@gmail.com>
2022-12-19 11:30:32 +0100

    dbscript for add NCI_FK_ProdOrderPartslistPos_ACClassTaskID index

---
+++ gip.mes.datamodel/DbScripts/iPlusMES/dbsync_2022-12-19_11-30_aagincic.sql
@@ -0,0 +1,9 @@
+IF (SELECT COUNT(*) FROM sys.indexes indexes INNER JOIN sys.objects 
+objects ON indexes.object_id = objects.object_id WHERE indexes.name 
+='NCI_FK_ProdOrderPartslistPos_ACClassTaskID' AND objects.name = 'ProdOrderPartslistPos') = 0
+	BEGIN
+	CREATE NONCLUSTERED INDEX [NCI_FK_ProdOrderPartslistPos_ACClassTaskID] ON [dbo].[ProdOrderPartslistPos]
+	(
+		[ACClassTaskID] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+END
