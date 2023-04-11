8776ec5fdd0326eb51ada93b68ea7dae566ace84
aagincic <aleksandar.agincic@gmail.com>
2023-04-11 15:45:47 +0200

    xd

---
+++ gip.bso.masterdata/Partslist/RuleItem.cs
@@ -0,0 +1,64 @@
+﻿using gip.core.datamodel;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace gip.bso.masterdata
+{
+    [ACClassInfo(Const.PackName_VarioMaterial, "en{'RuleItem'}de{'RuleItem'}", Global.ACKinds.TACObject, Global.ACStorableTypes.NotStorable, true, true)]
+    public class RuleItem : INotifyPropertyChanged
+    {
+        public RuleSelection RuleSelection { get; private set; }
+
+        public RuleItem(RuleSelection ruleSelection)
+        {
+            RuleSelection = ruleSelection;
+        }
+
+
+        [ACPropertyInfo(100, "", Const.ProcessModule)]
+        public ACClass Machine { get; set; }
+
+        private bool _IsSelected;
+
+        [ACPropertyInfo(101, "", gip.mes.datamodel.ConstApp.Select)]
+        public bool IsSelected
+        {
+            get
+            {
+                return _IsSelected;
+            }
+            set
+            {
+                _IsSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
+                if (RuleSelection.PropagateRuleSelection)
+                {
+                    RuleSelection.RuleGroup.PropagateSelection(RuleSelection, Machine, _IsSelected);
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region Metods
+
+        public override string ToString()
+        {
+            return $"[{(IsSelected ? "X" : "")}] {Machine?.ACCaption}";
+        }
+
+        #endregion
+    }
+}
