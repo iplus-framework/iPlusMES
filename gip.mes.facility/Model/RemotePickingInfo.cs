108644ef282c37f88c462972c6371179ec97eae6
aagincic <aleksandar.agincic@gmail.com>
2024-01-22 13:27:54 +0100

    PP test project cleanup

---
+++ gip.mes.facility/Model/RemotePickingInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace gip.mes.facility
+{
+    public class RemotePickingInfo
+    {
+        public long RowNr { get; set; }
+
+        public string PickingNo { get; set; }
+        public DateTime DeliveryDateFrom { get; set; }
+        public DateTime InsertDate { get; set; }
+        public string InsertName { get; set; }
+        public DateTime? PosStartUpdate { get; set; }
+        public DateTime? PosEndUpdate { get; set; }
+        public DateTime? FBStartUpdate { get; set; }
+        public DateTime? FBEndUpdate { get; set; }
+
+        public bool? IsSuccessfullyClosed { get; set; }
+    }
+}
