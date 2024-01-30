1a850c8d4e760f48f6dd30ec305a261cc8c36906
Ivan Hrastinski <ihrastinski@kajbum.com>
2024-01-30 13:24:31 +0100

    Dewiback and PWNode lab order

--- gip.mes.facility/Manager/ACLabOrderManager.cs
+++ gip.mes.facility/Manager/ACLabOrderManager.cs
@@ -2,8 +2,10 @@
 using gip.core.datamodel;
 using gip.mes.datamodel;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace gip.mes.facility
 {
@@ -128,5 +130,161 @@ namespace gip.mes.facility
 
         }
 
+
+        public Msg GetOrCreateLabTemplateForPWNode(DatabaseApp dbApp, string templateName, Material material, ACMethod pwNodeACMethod, IEnumerable<string> acIdentifiersOfExcludedResult, out LabOrder template)
+        {
+            template = null;
+
+            ACValueList resultList = pwNodeACMethod.ResultValueList;
+            if (resultList == null || !resultList.Any())
+                return null;
+
+            template = dbApp.LabOrder.FirstOrDefault(c => c.LabOrderTypeIndex == (short)GlobalApp.LabOrderType.Template
+                                                       && c.TemplateName == templateName
+                                                       && c.MaterialID == material.MaterialID);
+            if (template == null)
+            {
+                string secondaryKey = Root.NoManager.GetNewNo(Database, typeof(LabOrder), LabOrder.NoColumnName, LabOrder.FormatNewNo, this);
+                template = LabOrder.NewACObject(dbApp, null, secondaryKey);
+                template.LabOrderTypeIndex = (short)GlobalApp.LabOrderType.Template;
+                template.MDLabOrderState = dbApp.MDLabOrderState.FirstOrDefault(c => c.IsDefault);
+                template.TemplateName = templateName;
+                template.Material = material;
+                dbApp.LabOrder.AddObject(template);
+
+            }
+            
+            foreach (ACValue resultItem in resultList)
+            {
+                if (acIdentifiersOfExcludedResult != null && acIdentifiersOfExcludedResult.Contains(resultItem.ACIdentifier))
+                    continue;
+
+                MDLabTag resultItemTag = dbApp.MDLabTag.FirstOrDefault(c => c.MDKey == resultItem.ACIdentifier);
+                if (resultItemTag == null)
+                {
+                    resultItemTag = MDLabTag.NewACObject(dbApp, null);
+                    resultItemTag.MDKey = resultItem.ACIdentifier;
+                    resultItemTag.MDLabTagIndex = (short)MDLabTag.LabTags.Maesure;
+                    resultItemTag.MDNameTrans = resultItem.ACCaptionTrans;
+                    dbApp.MDLabTag.AddObject(resultItemTag);
+                }
+
+                LabOrderPos pos = template.LabOrderPos_LabOrder.FirstOrDefault(c => c.MDLabTagID == resultItemTag.MDLabTagID);
+                if (pos == null)
+                {
+                    pos = LabOrderPos.NewACObject(dbApp, template);
+                    pos.MDLabTag = resultItemTag;
+                    dbApp.LabOrderPos.AddObject(pos);
+                }
+            }
+
+            Msg msg = dbApp.ACSaveChanges();
+            if (msg != null)
+                return msg;
+            
+            return null;
+        }
+
+        public Msg CreateOrUpdateLabOrderForPWNode(DatabaseApp dbApp, string templateName, ACMethod pwNodeACMethod, ProdOrderPartslistPos intermediateChildPos, 
+                                                   ProdOrderPartslistPos intermediatePosition, ProdOrderPartslistPos endBatchPos, 
+                                                   IEnumerable<string> acIdentifersOfRepeatedResult, IEnumerable<string> acIdentifiersOfExcludedResult)
+        {
+            ACValueList resultList = pwNodeACMethod.ResultValueList;
+            if (resultList == null || !resultList.Any())
+                return null;
+
+
+            Material material = null;
+            if (endBatchPos != null)
+                material = endBatchPos.BookingMaterial;
+            if (material == null)
+                material = intermediatePosition.BookingMaterial;
+            if (material == null)
+            {
+                return new Msg(eMsgLevel.Error, "The material for laboratory order is not available!");
+            }
+
+            LabOrder template;
+
+            Msg msg = GetOrCreateLabTemplateForPWNode(dbApp, templateName, material, pwNodeACMethod, acIdentifiersOfExcludedResult, out template);
+            if (template == null && msg != null)
+            {
+                //TODO: details
+                return new Msg(eMsgLevel.Error, "The laboratory order template is not available!");
+            }
+
+            LabOrder currentLabOrder = dbApp.LabOrder.FirstOrDefault(c => c.ProdOrderPartslistPosID == intermediateChildPos.ProdOrderPartslistPosID);
+
+            if (currentLabOrder == null)
+            {
+                msg = CreateNewLabOrder(dbApp, template, "", null, null, intermediateChildPos, null, out currentLabOrder);
+                if (msg != null)
+                    return msg;
+            }
+
+            foreach (ACValue resultItem in resultList)
+            {
+                if (acIdentifiersOfExcludedResult != null && acIdentifiersOfExcludedResult.Contains(resultItem.ACIdentifier))
+                    continue;
+
+                double? result = null;
+
+                try
+                {
+                    result = resultItem.ParamAsDouble;
+                }
+                catch (Exception e)
+                {
+                    Messages.LogException(this.GetACUrl(), nameof(CreateOrUpdateLabOrderForPWNode), e);
+                }
+
+                if (!result.HasValue)
+                    continue;
+
+                LabOrderPos loPos = currentLabOrder.LabOrderPos_LabOrder.OrderByDescending(c => c.Sequence).FirstOrDefault(c => c.MDLabTag.MDKey == resultItem.ACIdentifier);
+                if (loPos != null && !(acIdentifersOfRepeatedResult != null && acIdentifersOfRepeatedResult.Contains(resultItem.ACIdentifier)))
+                {
+                    loPos.ActualValue = result;
+                }
+                else
+                {
+                    LabOrderPos templatePos = template.LabOrderPos_LabOrder.FirstOrDefault(c => c.MDLabTag.MDKey == resultItem.ACIdentifier);
+                    if (templatePos != null)
+                        loPos = LabOrderPos.NewACObject(dbApp, currentLabOrder, templatePos);
+
+                    int nextSequence = currentLabOrder.LabOrderPos_LabOrder.Max(c => c.Sequence) + 1;
+                    
+                    if (loPos != null)
+                    {
+                        loPos.Sequence = nextSequence;
+                        loPos.ActualValue = result;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public Msg CompleteLabOrderForPWNode(DatabaseApp dbApp, ProdOrderPartslistPos intermediateChildPos, ProdOrderPartslistPos intermediatePosition, ProdOrderPartslistPos endBatchPos)
+        {
+            Material material = null;
+            if (endBatchPos != null)
+                material = endBatchPos.BookingMaterial;
+            if (material == null)
+                material = intermediatePosition.BookingMaterial;
+            if (material == null)
+                return new Msg(eMsgLevel.Error, "The material is not assigned!");
+
+            MDLabOrderState mdLabOrderState = dbApp.MDLabOrderState.FirstOrDefault(c => c.MDLabOrderStateIndex == (short)MDLabOrderState.LabOrderStates.Finished);
+            if (mdLabOrderState != null)
+            {
+                LabOrder currentLabOrder = dbApp.LabOrder.FirstOrDefault(c => c.ProdOrderPartslistPosID == intermediateChildPos.ProdOrderPartslistPosID);
+                if (currentLabOrder != null)
+                    currentLabOrder.MDLabOrderState = mdLabOrderState;
+
+                return dbApp.ACSaveChanges();
+            }
+            return null;
+        }
     }
 }
