bb3914cc647cea1a186d71925718b2ddac9b0d89
aagincic <aleksandar.agincic@gmail.com>
2024-02-26 14:02:37 +0100

    iP-T-24-02-26-001 SchaefersLE in Work Center show ExternLotNo in lot selection

--- gip.bso.manufacturing/Model/WorkCenter/FacilityChargeItem.cs
+++ gip.bso.manufacturing/Model/WorkCenter/FacilityChargeItem.cs
@@ -2,9 +2,6 @@
 using gip.mes.datamodel;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace gip.bso.manufacturing
 {
