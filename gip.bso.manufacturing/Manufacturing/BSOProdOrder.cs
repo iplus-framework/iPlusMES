4aa25533f1ecb7402301fad75a11a8d72a45f046
Damir Lisak <dlisak@kajbum.com>
2024-11-02 16:44:42 +0100

    Copyright added

--- gip.bso.manufacturing/Manufacturing/BSOProdOrder.cs
+++ gip.bso.manufacturing/Manufacturing/BSOProdOrder.cs
@@ -1,3 +1,5 @@
+// Copyright (c) 2024, gipSoft d.o.o.
+// Licensed under the GNU GPLv3 License. See LICENSE file in the project root for full license information.
 using gip.bso.facility;
 using gip.bso.masterdata;
 using gip.core.autocomponent;
