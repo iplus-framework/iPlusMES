7b75cb048d645a9887c7d04fadab10a93d00674f
aagincic <aleksandar.agincic@gmail.com>
2025-05-05 14:57:22 +0200

    iP-T-25-05-05-001 │ BSOProcessControlVB add MaterialNo to explorer

--- gip.mes.processapplication/BSO/ACClassTaskModel.cs
+++ gip.mes.processapplication/BSO/ACClassTaskModel.cs
@@ -6,23 +6,26 @@ namespace gip.mes.processapplication
     [ACClassInfo(Const.PackName_VarioAutomation, "en{'ACClassTaskModel'}de{'ACClassTaskModel'}", Global.ACKinds.TACClass, Global.ACStorableTypes.NotStorable)]
     public class ACClassTaskModel
     {
-        [ACPropertyInfo(7, "ShowCol", "en{'Order-No.'}de{'Auftrag-Nr.'}")]
+        [ACPropertyInfo(1, "ShowCol", "en{'Order-No.'}de{'Auftrag-Nr.'}")]
         public string ProgramNo { get; set; }
 
 
-        [ACPropertyInfo(8, "ShowCol", "en{'Batch-No.'}de{'Batch-Nr.'}")]
+        [ACPropertyInfo(2, "ShowCol", "en{'Batch-No.'}de{'Batch-Nr.'}")]
         public string BatchNo { get; set; }
 
-        [ACPropertyInfo(9, "ShowCol", "en{'Material'}de{'Material'}")]
-        public string Material { get; set; }
+        [ACPropertyInfo(3, "ShowCol", "en{'Material No.'}de{'Material-Nr.'}")]
+        public string MaterialNo { get; set; }
 
-        [ACPropertyInfo(10, "ShowCol", "en{'Startet'}de{'Gestartet'}")]
+        [ACPropertyInfo(3, "ShowCol", "en{'Material name'}de{'Materialname'}")]
+        public string MaterialName { get; set; }
+
+        [ACPropertyInfo(5, "ShowCol", "en{'Startet'}de{'Gestartet'}")]
         public DateTime InsertDate { get; set; }
 
-        [ACPropertyInfo(12, "ShowCol", "en{'Program-No'}de{'Programm-Nr.'}")]
+        [ACPropertyInfo(6, "ShowCol", "en{'Program-No'}de{'Programm-Nr.'}")]
         public string ACProgramNo { get; set; }
 
-        [ACPropertyInfo(11, "ShowCol", "en{'Id'}de{'Id'}")]
+        [ACPropertyInfo(7, "ShowCol", "en{'Id'}de{'Id'}")]
         public string ACIdentifier { get; set; }
 
         public Guid ACClassTaskID { get; set; }
