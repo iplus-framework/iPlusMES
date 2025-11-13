58a4ba8e67899b5d3d12804afb0181d1cf862705
Damir Lisak <dlisak@kajbum.com>
2025-11-13 18:51:42 +0100

    Build Directives added for switching between avalonia pacakages and source to enable Compiling and debugging with avalonia source

--- gip.mes.datamodel/EntityInvoice/Invoice.cs
+++ gip.mes.datamodel/EntityInvoice/Invoice.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
-using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace gip.mes.datamodel
