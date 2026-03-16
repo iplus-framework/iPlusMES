aabcc5ce48659bac03f573baadf2ee9ac79c2e60
Aleksandar Agincic <aleksandar.agincic@iplus-framework.com>
2026-03-16 12:19:59 +0100

    BSOTandTFastView bug fix - open ProdOrder dialog not receive correct parameters

--- gip.bso.facility/FacilityHistory/BSOTandTFastView.cs
+++ gip.bso.facility/FacilityHistory/BSOTandTFastView.cs
@@ -477,6 +477,9 @@ namespace gip.bso.facility
                         .DefaultIfEmpty()
                         .FirstOrDefault(),
 
+                    ProdOrderPartslistID = c.ProdOrderPartslistID,
+                    ProductionDate = (c.StartDate != null ? c.StartDate.Value : c.InsertDate),
+
                     MaterialNo = c.Partslist.Material.MaterialNo,
                     MaterialName = c.Partslist.Material.MaterialName1,
                     TargetActualQuantityUOM = c.ActualQuantity,
